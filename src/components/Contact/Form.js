e773498f3c27e863abdd9a9715403dab45fd8bab
sarozpradhan64 <sarozpradhan64@gmail.com>
2021-06-16 21:32:36 +0545

    commit

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -3,15 +3,15 @@ import './contact.css'
 function Form() {
     return (
        
-            <form name="contact" method="POST" data-netlify="true">
+            <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
                 <input type="hidden" name="form-name" value="contact"/>
                 <input className="name" type="text" id="name" name="name" placeholder="Enter your name"/> 
                 <input type="text" id="phone" name="phone" placeholder="Phone number"/>
                 <input type="email" id="email" name ="email" placeholder="Enter your email address"/>
                 <textarea id="message" name="message" placeholder="Your message"></textarea>
-                <input className="submit" type="submit" value="Send"/>
-            </form>,
-            document.getElementById('root')
+                <button className="submit" type="submit">Send</button>
+            </form>
+            
        
     )
 }
