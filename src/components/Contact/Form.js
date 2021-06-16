289d283b31c6be4b20d372bde9d759518363ca9a
sarozpradhan64 <sarozpradhan64@gmail.com>
2021-06-16 19:40:46 +0545

    netlify

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -3,7 +3,7 @@ import './contact.css'
 function Form() {
     return (
         <div>
-            <form netlify>
+            <form method="POST" data-netlify="true">
                 <input className="name" type="text" placeholder="Enter your name"/> 
                 <input type="text" placeholder="Phone number"/>
                 <input type="text" placeholder="Enter your email address"/>
