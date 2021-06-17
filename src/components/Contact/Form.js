70ee9106cc04791db009605d3578e1858777582e
sarozpradhan64 <sarozpradhan64@gmail.com>
2021-06-17 15:17:59 +0545

    commit

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -1,31 +1,54 @@
-import React, {useState} from 'react'
-import './contact.css'
+import React, { useState } from "react";
+import "./contact.css";
+import Success from "./Success";
 function Form() {
-  
-    return (
-       <div className="form_container">
-           
-            <form name="contact"
-            method="POST"
-            action="/success"
-            data-netlify="true" 
-            onSubmit = "submit"
-            data-netlify-honeypot ="bot-field">
-                <input type="hidden" name="form-name" value="contact"/>
-                <div hidden>
-                    <input name="bot-field"/>
-                </div>
-                <input className="name" type="text" id="name" name="name" placeholder="Enter your name" required/> 
-                <input type="number" id="phone" name="phone" placeholder="Enter your Phone number" />
-                <input type="email" id="email" name ="email" placeholder="Enter your email address"/>
-                <textarea id="message" name="message" placeholder="Your message" required></textarea>
-                <button className="submit" type="submit">Send</button>
-            
-            </form>
-            </div>
-       
-    )
+  return (
+    <div className="form_container">
+      <form
+        name="contact"
+        action="/success"
+        data-netlify="true"
+        onSubmit="submit"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <div hidden>
+          <input name="bot-field" />
+        </div>
+        
+        <input
+          className="name"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter your name"
+          onChange={handleClick}
+          required
+        />
+        <input
+          type="number"
+          id="phone"
+          name="phone"
+          placeholder="Enter your Phone number"
+        />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email address"
+        />
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Your message"
+          required
+        ></textarea>
+        <button className="submit" type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
-
 
 export default Form;
--- src/components/Contact/Success.js
+++ src/components/Contact/Success.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom' 
-import './contact.css'
-import Form from './Form'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./contact.css";
+import Form from "./Form";
 function Success() {
-  
-    return (
-        
-        <div className="form_container">
-            
-        <form>
-       
-       <p>Your message is sent successfully</p>
-       <NavLink exact to="./contact" className="back">Back</NavLink>
-        </form>
-        </div>
-    )
+  return (
+    <div className="form_container">
+      <form>
+        <p>Your message is sent successfully</p>
+        <NavLink exact to="./contact" className="back">
+          Back
+        </NavLink>
+      </form>
+    </div>
+  );
 }
 
-export default Success
+export default Success;
