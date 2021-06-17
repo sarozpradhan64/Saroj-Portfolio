87fd9a80278316ba0190b1942b86e94e07c7e5dc
sarozpradhan64 <sarozpradhan64@gmail.com>
2021-06-17 08:30:09 +0545

    netlify form

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -3,8 +3,13 @@ import './contact.css'
 function Form() {
     return (
        
-            <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+            <form name="contact" method="POST" 
+            data-netlify="true" onSubmit="submit" 
+            data-netlify-honeypot ="bot-field">
                 <input type="hidden" name="form-name" value="contact"/>
+                <div hidden>
+                    <input name="bot-field"/>
+                </div>
                 <input className="name" type="text" id="name" name="name" placeholder="Enter your name"/> 
                 <input type="text" id="phone" name="phone" placeholder="Phone number"/>
                 <input type="email" id="email" name ="email" placeholder="Enter your email address"/>
