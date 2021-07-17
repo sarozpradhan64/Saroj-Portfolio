3c3da529199e9b01e948082e23f4ea07407390e1
sarozpradhan64 <sarozpradhan64@gmail.com>
2021-07-17 12:33:39 +0545

    react-router repair

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Scrolltotop from "./Scrolltotop";
 import Home from "./components/Home/Home";
 import Project from "./components/Projects/Projects";
 import Blog from "./components/Blogs/Blog";
 import Gallery from "./components/Gallery/Gallery";
 import Footer from "./components/footer";
-import Form from "./components/Contact/Form";
 import NotFoundpage from "./components/NotFoundpage";
+import Form from "./components/Contact/Form";
 import Success from "./components/Contact/Success";
-import Category from './components/Blogs/Category';
+import Category from "./components/Blogs/Category";
 import Blogdetail from "./components/Blogs/Blogdetail";
-/* blog routes */ 
+
+/* blog routes */
 function App() {
   return (
     <>
       <Router>
-        
-        <Navbar />{" "}
-        {/* navbar and footer outside the switch tags means that they will be present in all the pages */}
+        <Navbar />
+         <Scrolltotop />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route exact path="/projects/" component={Project}/>
-          <Route exact path="/gallery/" component={Gallery}/>
-          <Route exact path="/contact/" component={Form}/>
-          <Route exact path="/success/" component={Success}/>
-          <Route exact path="/blog" component={Blog}/>
-          <Route exact path = "/category/:id" component={Category} />
-          <Route exact path = '/blog/:id' component={Blogdetail}/>
+          <Route exact path="/projects/" component={Project} />
+          <Route exact path="/gallery/" component={Gallery} />
+          <Route exact path="/contact/" component={Form} />
+          <Route exact path="/success/" component={Success} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/category/:id" component={Category} />
+          <Route exact path="/blog/:id" component={Blogdetail} />
           <Route path="*" component={NotFoundpage} />
-
-         
         </Switch>
         <Footer />
       </Router>
-     
     </>
   );
 }
