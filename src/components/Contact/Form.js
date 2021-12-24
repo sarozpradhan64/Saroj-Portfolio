cf5a5412989153a74e0afc1b2beefbf806d5c641
sarozpradhan64 <sarozpradhan64@gmail.com>
2021-12-24 21:46:37 +0545

    contact form

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 import emailjs from "emailjs-com";
 import Success from "./Success";
 import { useCookies } from "react-cookie";
 function Form() {
 
+  const form = useRef();
 
   function sendEmail(e) {
-   
-    emailjs.sendForm("gmail", "template_94vaouj", e.target, "user_62jnS4jHOrdWu9kznFXvX")
+   e.preventDefault();
+    emailjs.sendForm("gmail", "template_94vaouj", form.current, "user_62jnS4jHOrdWu9kznFXvX")
       .then(
         (result) => {
           console.log(result.text);
@@ -20,7 +21,8 @@ function Form() {
   }
   return (
     <div className="form_container">
-      <form className="contact-form" action="/success">
+      <form ref={form} onSubmit={sendEmail} className="contact-form" action="/success">
+        <h2 className="form_title">Send me a Message</h2>
         <input type="hidden" name="contact_number" />
         <div hidden>
          
